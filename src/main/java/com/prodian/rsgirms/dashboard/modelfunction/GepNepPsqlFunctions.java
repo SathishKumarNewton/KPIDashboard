3909ce0de84d49a22dcb1c9d94c1aa765c32714e
userrohit28 <rohitlnze09@gmail.com>
2022-08-05 18:54:02 +0530

    gepnep added

---
+++ src/main/java/com/prodian/rsgirms/dashboard/modelfunction/GepNepPsqlFunctions.java
@@ -0,0 +1,18 @@
+package com.prodian.rsgirms.dashboard.modelfunction;
+
+public interface GepNepPsqlFunctions {
+
+	
+	double getGepsum();
+	double getGepodsum();
+	double getGeptpsum();
+	double getNepsum();
+	double getNepodsum();
+	double getNeptpsum();
+	
+	
+	
+
+	
+	
+}
--- src/main/java/com/prodian/rsgirms/dashboard/rsrepository/GicNicPsqlRepository.java
+++ src/main/java/com/prodian/rsgirms/dashboard/rsrepository/GicNicPsqlRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.prodian.rsgirms.dashboard.modelfunction.GepNepPsqlFunctions;
 import com.prodian.rsgirms.dashboard.modelfunction.UserRole;
 
 import java.io.Serializable;
@@ -13,4 +14,12 @@ public interface GicNicPsqlRepository extends JpaRepository<UserRole, Serializab
 
     // @Query("select * from calc_new_r12_gic_nic_now(' and (BUSINESS_TYPE) in (''Renewal'') and (CHANNEL) in (''OEM'') and (SUB_CHANNEL) in (''Honda Assure'') and (MAKE) in (''Honda Motors Ltd.'') and (MODELGROUP) in (''WRV'') and upper(coalesce(FUELTYPE,''N'')) in (''DIESEL'') and (STATEGROUPING) in (''Rest of Tamilnadu'')  and (ncb_flag) in (''N'')','201904')")
     // GicNicPsqlFunction callR12GicNic();
+	
+	@Query(value="select * from calc_new_r12_gep_nep_now(' and (BUSINESS_TYPE) in (''Renewal'') and (CHANNEL) in (''OEM'') and (SUB_CHANNEL) in (''Honda Assure'') and (MAKE) in (''Honda Motors Ltd.'') and (MODELGROUP) in (''WRV'') and upper(coalesce(FUELTYPE,''N'')) in (''DIESEL'') and (STATEGROUPING) in (''Rest of Tamilnadu'')  and (ncb_flag) in (''N'')','201904')"
+)
+	GepNepPsqlFunctions callR12GepNep();
+	
+	
+	
+	
 }
