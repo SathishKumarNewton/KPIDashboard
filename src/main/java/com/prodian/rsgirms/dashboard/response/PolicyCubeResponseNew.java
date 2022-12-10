6541687e46dd13404bd7492bb5d1b6211241bd47
Salvation-R <salvation.r@prodian.co.in>
2022-12-10 09:27:54 +0530

    CallpolicyApicompleted

--- src/main/java/com/prodian/rsgirms/dashboard/response/PolicyCubeResponseNew.java
+++ src/main/java/com/prodian/rsgirms/dashboard/response/PolicyCubeResponseNew.java
@@ -15,10 +15,18 @@ public class PolicyCubeResponseNew {
 	private double writtenPoliciesTp;
 	private double writtenPoliciesOthers;
 	private double writtenPolicies;
+	private double addonWrittenPoliciesComprehensive;
+	private double addonWrittenPoliciesTp;
+	private double addonWrittenPoliciesOthers;
+	private double addonWrittenPolicies;
 	private double acqCostComprehensive;
 	private double acqCostTp;
 	private double acqCostOthers;
 	private double acqCost;
+	private double addonAcqCostComprehensive;
+	private double addonAcqCostTp;
+	private double addonAcqCostOthers;
+	private double addonAcqCost;
 	private double livesCovered;
 	
 	
